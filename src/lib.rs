d6c5955842b5390d12edb6beb1f45388e1bc3a63
Y5 <76672645+yugonsan@users.noreply.github.com>
2024-07-12 11:19:38 +0900

    dek

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,3 @@ pub use witness::WitnessCalculator;
 
 pub mod circom;
 pub use circom::{CircomBuilder, CircomCircuit, CircomConfig, CircomReduction};
-
-#[cfg(feature = "ethereum")]
-pub mod ethereum;
-
-mod zkey;
-pub use zkey::read_zkey;
